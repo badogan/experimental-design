11389ea08c64312b338f2fca6468e32163e2ef7d
badogan <basri.dogan.71@gmail.com>
2020-02-14 18:13:51 +0000

    initial research

---
+++ experimental-design/src/testingGooglePlaces.js
@@ -0,0 +1,8 @@
+let service = new google.maps.places.PlacesService(map)
+let request = {
+  location: new google.maps.LatLng(-33.8736510, 151.20688960),
+  radius: 500
+};
+service.search(request,function(results){
+  console.log(results)
+})
